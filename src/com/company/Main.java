54c19051a90b1cbf22b588ba1042533c82cad103
alumne1daw <helbaz1993@gmail.com>
2018-05-23 17:35:19 +0200

    Version Alpha

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import acm.program.GraphicsProgram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new App().start();
+    }
+
+    public static class App extends GraphicsProgram {
+        public final void run() {
+
+        }
+    }
+}
